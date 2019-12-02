dcb1dfd30fb994ad5c9b4c51b6daa69880a336f2
Sajed Nahian <contactsajednahian@gmail.com>
2019-12-01 21:54:47 -0500

    sync

--- src/Components/Technology/Technology.tsx
+++ src/Components/Technology/Technology.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import './Technology.scss';
 
-<<<<<<< HEAD
 interface Props {
   imageUrl: string;
 }
@@ -10,14 +9,6 @@ export const Technology: React.FC<Props> = ({ imageUrl }) => {
   return (
     <a className="technology">
       <img src={imageUrl} alt="" />
-=======
-interface Props {}
-
-export const Technology: React.FC<Props> = () => {
-  return (
-    <a href="">
-      <img src="https://cdn.auth0.com/blog/react-js/react.png" alt="" />
->>>>>>> fed1e85b4c916d30af13efec5b40cc3c05708027
     </a>
   );
 };
